825f5a54a10ce30463064f487bd4ddf3212a2eba
Tachyrk <65167116+Tachyrk@users.noreply.github.com>
2026-08-22 23:56:46 +0800

    Add: design-word-search-data-structure - submission-3

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-3.cpp
@@ -0,0 +1,60 @@
+class WordDictionary {
+public:
+    struct TRIE{
+        TRIE* child[26];
+        bool isEnd;
+        TRIE(){
+            for(int i = 0; i < 26; i++){
+                child[i] = nullptr;
+            }
+            isEnd = false;
+        }
+        ~TRIE(){
+            for(int i = 0; i < 26; i++){
+                delete child[i];
+            }
+        }
+    };
+
+    TRIE* head;
+    WordDictionary() {
+        head = new TRIE();
+    }
+    
+    void addWord(string word) {
+        TRIE* current = head;
+        for(char c : word){
+            int next = c - 'a';
+            if(!current->child[next]){
+                current->child[next] = new TRIE();
+            }
+            current = current->child[next];
+        }
+        current->isEnd = true;
+    }
+    
+    bool search(string word) {        
+        auto dfs = [&](auto &self, TRIE* current, int idx) ->bool{
+            if(!current) return false;
+            if(idx == word.size() && current->isEnd) return true;
+            if(idx == word.size()) return false;
+            int next = word[idx] - 'a';
+            if(word[idx] == '.'){
+                for(int i = 0; i < 26; i++){
+                    if(current->child[i]){
+                       if(self(self, current->child[i], idx + 1)) return true;
+                    }
+                }
+            }else{                
+                if(current->child[next]){
+                    return self(self, current->child[next], idx + 1);
+                }else{
+                    return false;
+                }
+            }
+            return false;
+        };
+
+        return dfs(dfs, head, 0);
+    }
+};
