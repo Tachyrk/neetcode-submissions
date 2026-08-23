3ca1dcfc1d413e1ee3ec8353d1ba6aea03efdeff
Tachyrk <65167116+Tachyrk@users.noreply.github.com>
2026-08-23 12:28:08 +0800

    Add: min-cost-climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-4.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        int dp1 = 0, dp2 = 0;
+        for(int i = 2; i <= n; i++){
+            int temp = min(dp1 + cost[i - 1], dp2 + cost[i - 2]);
+            dp2 = dp1;
+            dp1 = temp;
+        }
+        return dp1;
+    }
+};
