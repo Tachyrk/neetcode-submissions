02d19e12f681a41c70323c9dc4e9a0ea0992541a
Tachyrk <65167116+Tachyrk@users.noreply.github.com>
2026-08-27 00:10:54 +0800

    Add: hand-of-straights - submission-2

---
+++ Data Structures & Algorithms/hand-of-straights/submission-2.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        int n = hand.size();
+        if(n % groupSize != 0) return false;
+        int remaingroups = n / groupSize;
+
+        int maxValue = INT_MIN;
+        int minValue = INT_MAX;
+        map<int, int> freq;
+        for(int i = 0; i < n; i++){
+            freq[hand[i]]++;
+            minValue = min(minValue, hand[i]);
+            maxValue = max(maxValue, hand[i]);
+        }
+
+        for(auto [key, value] : freq){
+            if(value > 0){
+                for(int j = key + 1; j < key + groupSize; j++){
+                    freq[j] -= value;
+                    if(freq[j] < 0){
+                        return false;
+                    }
+                }          
+            }     
+        }
+        return true;
+    }
+};
