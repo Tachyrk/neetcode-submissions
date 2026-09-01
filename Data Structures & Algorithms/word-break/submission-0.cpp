bea14d5d1c68ade6591d9ca25200eb951ec647a8
Tachyrk <65167116+Tachyrk@users.noreply.github.com>
2026-09-01 23:38:07 +0800

    Add: word-break - submission-0

---
+++ Data Structures & Algorithms/word-break/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n = s.size();
+        vector<bool> dp(n + 1, false);
+        dp[0] = true;
+        for(int i = 0; i < n; i++){
+            if(dp[i] == true){
+                for(auto & word : wordDict){
+                    int len = word.size();
+                    if(i + len > n) continue;
+                    if(s.compare(i, len, word) == 0){
+                        dp[i + len] = true;
+                    }
+                }
+            }
+        }
+
+        return dp[n];
+    }
+};
